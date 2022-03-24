3768b8dcf01b56798a26164183e3a5262609f620
Peeyush <peeyushchaudhary2404@gmail.com>
2022-03-24 10:37:05 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -41,7 +41,7 @@ function App() {
               <img src="/Images/img1.jpg" className="d-block w-100" alt="..." />
               <div className="carousel-caption d-none d-md-block">
                 <h5>First slide label</h5>
-                <p>Some representative placeholder content for the first slide.</p>
+                {/* <p>Some representative placeholder content for the first slide.</p> */}
               </div>
             </div>
             <div className="carousel-item">
